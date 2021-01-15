3769dec8aad7967f2c21cdf2e97faf2dbce92b29
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-01-15 14:50:42 +0100

    Update chocolatey_upgrade.bat

--- chocolatey_upgrade.bat
+++ chocolatey_upgrade.bat
@@ -3,12 +3,11 @@
 REM Tonton Jo - 2020
 REM Join me on Youtube: https://www.youtube.com/c/tontonjo
 
-REM Script to update all chocolatey installed applicationn
-REM Logs are written in user documents folder :C:\Users\username\Documents\choco_upgrade_log
+REM Script to update all chocolatey installed application
+REM Logs are written at root of c: drive: c:\choco_upgrade_log.txt
 
 REM Version 1.0
 
-echo START at %DATE% - %TIME% -------------------------------------------- >> "%HOMEPATH%\choco_upgrade_log.txt"
-
-choco upgrade -y all >> "%HOMEPATH%\choco_upgrade_log.txt"
-echo END ----------------------------------------------------------------- >> "%HOMEPATH%\choco_upgrade_log.txt"
+echo START at %DATE% - %TIME% -------------------------------------------- >> "c:\choco_upgrade_log.txt"
+choco upgrade -y all >> "c:\choco_upgrade_log.txt"
+echo END ----------------------------------------------------------------- >> "c:\choco_upgrade_log.txt"
